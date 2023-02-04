9872c780c735b9db612948c08b0b4e14f8a7eb46
marinadimovska <mdimovska08@gmail.com>
2023-02-04 16:37:21 +0200

    SoftUni-MySQL

---
+++ 01.Data Definition And Data Types/01.CreateTable.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE `minions`;
+USE `minions`;
+
+CREATE TABLE `minions` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(30),
+`age` int
+);
+
+CREATE TABLE `town`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(30)
+);
